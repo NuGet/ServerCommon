f59ee3ff36b51cf18f31636e4baa34842ce8b2a2
scottbommarito <scottbom@microsoft.com>
2017-01-20 11:04:39 -0800

    fix init.ps1 indentation

--- build/init.ps1
+++ build/init.ps1
@@ -16,34 +16,34 @@ if ($Branch) {
 }
 
 Function Download-Folder {
-	[CmdletBinding()]
-	param(
-		[string]$Path
-	)
-	
+    [CmdletBinding()]
+    param(
+        [string]$Path
+    )
+    
     $DirectoryPath = (Join-Path $NuGetClientRoot $FilePath)
-	if (-not (Test-Path $DirectoryPath)) {
-		New-Item -Path $DirectoryPath -ItemType "directory"
-	}
-	
+    if (-not (Test-Path $DirectoryPath)) {
+        New-Item -Path $DirectoryPath -ItemType "directory"
+    }
+    
     $FolderUri = "$RootGitHubApiUri/$Path$Ref"
     Write-Host "Downloading files from $FolderUri"
     $Files = wget $FolderUri | ConvertFrom-Json
     Foreach ($File in $Files) {
-		$FilePath = $File.path
-		if ($File.type -eq "file") {
-			$DownloadUrl = $File.download_url
-			Write-Host "Downloading file at $DownloadUrl"
-			wget -Uri $DownloadUrl -OutFile (Join-Path $NuGetClientRoot $FilePath)
-		} elseif ($File.type -eq "dir") {
-			Download-Folder -Path $FilePath
-		}
-	}
+        $FilePath = $File.path
+        if ($File.type -eq "file") {
+            $DownloadUrl = $File.download_url
+            Write-Host "Downloading file at $DownloadUrl"
+            wget -Uri $DownloadUrl -OutFile (Join-Path $NuGetClientRoot $FilePath)
+        } elseif ($File.type -eq "dir") {
+            Download-Folder -Path $FilePath
+        }
+    }
 }
 
 $FoldersToDownload = "build", "tools"
 foreach ($Folder in $FoldersToDownload) {
-	Download-Folder -Path $Folder
+    Download-Folder -Path $Folder
 }
 
 # Run common.ps1
