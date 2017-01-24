b954a30db59a1783304d3927a8003ee3b036f4c3
scottbommarito <scottbom@microsoft.com>
2017-01-24 11:15:36 -0800

    usebasicparsing

--- build/Queue-Build.ps1
+++ build/Queue-Build.ps1
@@ -13,7 +13,7 @@ $Base64AuthInfo = [Convert]::ToBase64String([Text.Encoding]::ASCII.GetBytes(("{0
 $Headers = @{Authorization=("Basic {0}" -f $Base64AuthInfo)}
 
 # Check if there is already a build queued, so we don't queue the same build multiple times.
-$ListResponse = Invoke-WebRequest -Method Get -Uri "https://$Instance.visualstudio.com/DefaultCollection/$Project/_apis/build/builds?api-version=2.0&definitions=$DefinitionId&statusFilter=notStarted" -Headers $Headers
+$ListResponse = Invoke-WebRequest -UseBasicParsing -Method Get -Uri "https://$Instance.visualstudio.com/DefaultCollection/$Project/_apis/build/builds?api-version=2.0&definitions=$DefinitionId&statusFilter=notStarted" -Headers $Headers
 $QueuedBuilds = ConvertFrom-Json $ListResponse
 Foreach ($QueuedBuild in $QueuedBuilds.value) {
     $QueuedBranch = $QueuedBuild.sourceBranch -replace 'refs\/heads\/', ''
@@ -28,5 +28,5 @@ Foreach ($QueuedBuild in $QueuedBuilds.value) {
 $QueueParameters = @{SubmoduleBranch=$SubmoduleBranch}
 $QueueBody = @{definition=@{id=$DefinitionId};sourceBranch=$Branch;parameters=(ConvertTo-Json $QueueParameters)}
 
-$QueueResponse = Invoke-RestMethod -Method Post -ContentType application/json -Uri "https://$Instance.visualstudio.com/DefaultCollection/$Project/_apis/build/builds?api-version=2.0" -Headers $Headers -Body (ConvertTo-Json $QueueBody)
+$QueueResponse = Invoke-RestMethod -UseBasicParsing -Method Post -ContentType application/json -Uri "https://$Instance.visualstudio.com/DefaultCollection/$Project/_apis/build/builds?api-version=2.0" -Headers $Headers -Body (ConvertTo-Json $QueueBody)
 Write-Host $QueueResponse
