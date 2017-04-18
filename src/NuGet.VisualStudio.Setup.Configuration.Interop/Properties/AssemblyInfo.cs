9aec252f2bf66daba419d0640b027dd74220858e
scottbommarito <scottbom@microsoft.com>
2017-04-18 16:37:31 -0700

    use msbuildpath everywhere, install nuget.visualstudio.setup.configuration.interop

---
+++ src/NuGet.VisualStudio.Setup.Configuration.Interop/Properties/AssemblyInfo.cs
@@ -0,0 +1,12 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.VisualStudio.Setup.Configuration.Interop")]
+[assembly: AssemblyDescription("Shared component to assist NuGet build scripts with finding Visual Studio installations.")]
+[assembly: AssemblyProduct("NuGet.VisualStudio.Setup.Configuration.Interop")]
+[assembly: AssemblyCompany(".NET Foundation")]
+[assembly: AssemblyCopyright("Copyright © .NET Foundation 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("7ebf325f-8671-46ef-b065-d9eb4c60b850")]
+
+// Assembly version info is set at build time
