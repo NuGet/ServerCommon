f41ebf870a78b40e385c56bb3d1fce846a3d2b48
Andrei Grigorev <angrigor@microsoft.com>
2017-05-17 22:59:14 -0700

    Added ForceSslMiddleware and tests

---
+++ src/NuGet.Services.ForceSslMiddleware/Properties/AssemblyInfo.cs
@@ -0,0 +1,11 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("NuGet.Services.ForceSslMiddleware")]
+[assembly: AssemblyDescription("Owin middleware to force use of HTTPS")]
+[assembly: AssemblyCompany(".NET Foundation")]
+[assembly: AssemblyProduct("NuGet.Services.ForceSslMiddleware")]
+[assembly: AssemblyCopyright("Copyright © .NET Foundation 2017")]
+[assembly: ComVisible(false)]
+[assembly: Guid("5af3ce0b-9c47-4fd0-83f6-d6c1e0a50e85")]
