9f891e46350f9d2561b4f96c917ba0a6632bd68f
Loic Sharma <sharma.loic@gmail.com>
2017-05-22 11:10:50 -0700

    Fixed usings

--- src/NuGet.Services.Logging/TelemetryResponseCodeProcessor.cs
+++ src/NuGet.Services.Logging/TelemetryResponseCodeProcessor.cs
@@ -2,10 +2,10 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.ApplicationInsights.Extensibility;
+using System.Collections.Generic;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DataContracts;
-using System.Collections.Generic;
+using Microsoft.ApplicationInsights.Extensibility;
 
 namespace NuGet.Services.Logging
 {
