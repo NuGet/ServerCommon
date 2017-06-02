a7f0a698b634656ce84646ba7212c641e66fbe83
Damon Tivel <dtivel@microsoft.com>
2017-06-02 06:20:31 -0700

    Reduce exception telemetry noise

--- src/NuGet.Services.Logging/TelemetryResponseCodeProcessor.cs
+++ src/NuGet.Services.Logging/TelemetryResponseCodeProcessor.cs
@@ -3,9 +3,13 @@
 
 using System;
 using System.Collections.Generic;
+using System.Web;
 using Microsoft.ApplicationInsights.Channel;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace NuGet.Services.Logging
 {
@@ -29,18 +33,91 @@ namespace NuGet.Services.Logging
 
         public void Process(ITelemetry item)
         {
-            var request = item as RequestTelemetry;
-            int responseCode;
-
-            if (request != null && int.TryParse(request.ResponseCode, out responseCode))
+            if (item == null)
             {
-                if (SuccessfulResponseCodes.Contains(responseCode))
-                {
-                    request.Success = true;
-                }
+                throw new ArgumentNullException(nameof(item));
+            }
+
+            ITelemetry nextItem = null;
+
+            if (TryProcess(item as RequestTelemetry))
+            {
+            }
+            else if (TryProcess(item as ExceptionTelemetry, out nextItem))
+            {
+                item = nextItem;
             }
 
             _next.Process(item);
+        }
+
+        private bool TryProcess(RequestTelemetry telemetry)
+        {
+            int responseCode;
+
+            if (telemetry != null && int.TryParse(telemetry.ResponseCode, out responseCode))
+            {
+                if (SuccessfulResponseCodes.Contains(responseCode))
+                {
+                    telemetry.Success = true;
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static bool TryProcess(ExceptionTelemetry telemetry, out ITelemetry nextTelemetry)
+        {
+            nextTelemetry = telemetry;
+
+            if (telemetry != null)
+            {
+                var httpException = telemetry.Exception as HttpException;
+
+                if (httpException != null)
+                {
+                    var httpCode = httpException.GetHttpCode();
+
+                    if (httpCode < 500)
+                    {
+                        // Logging exception telemetry for a request that resulted in an HTTP response code under 500
+                        // adds noise to exception telemetry analysis.  Log it as trace telemetry instead.
+                        nextTelemetry = Convert(telemetry, httpException);
+                    }
+
+                    return true;
+                }
+            }
+
+            return false;
+        }
+
+        private static TraceTelemetry Convert(ExceptionTelemetry exceptionTelemetry, HttpException exception)
+        {
+            var traceTelemetry = new TraceTelemetry(exception.Message, SeverityLevel.Warning);
+
+            traceTelemetry.Context.Cloud.RoleInstance = exceptionTelemetry.Context.Cloud.RoleInstance;
+            traceTelemetry.Context.Cloud.RoleName = exceptionTelemetry.Context.Cloud.RoleName;
+            traceTelemetry.Context.GetInternalContext().SdkVersion = exceptionTelemetry.Context.GetInternalContext().SdkVersion;
+            traceTelemetry.Context.InstrumentationKey = exceptionTelemetry.Context.InstrumentationKey;
+            traceTelemetry.Context.Operation.Id = exceptionTelemetry.Context.Operation.Id;
+            traceTelemetry.Context.Operation.Name = exceptionTelemetry.Context.Operation.Name;
+            traceTelemetry.Context.Operation.ParentId = exceptionTelemetry.Context.Operation.ParentId;
+            traceTelemetry.Context.Location.Ip = exceptionTelemetry.Context.Location.Ip;
+
+            foreach (var property in exceptionTelemetry.Properties)
+            {
+                traceTelemetry.Properties.Add(property.Key, property.Value);
+            }
+
+            traceTelemetry.Properties.Add("exception", JObject.FromObject(exception).ToString(Formatting.None));
+
+            traceTelemetry.Sequence = exceptionTelemetry.Sequence;
+            traceTelemetry.Timestamp = exceptionTelemetry.Timestamp;
+
+            return traceTelemetry;
         }
     }
 }
--- tests/NuGet.Services.Logging.Tests/TelemetryResponseCodeProcessorFacts.cs
+++ tests/NuGet.Services.Logging.Tests/TelemetryResponseCodeProcessorFacts.cs
@@ -2,95 +2,258 @@
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
 using System;
-using Microsoft.ApplicationInsights.Channel;
+using System.Web;
 using Microsoft.ApplicationInsights.DataContracts;
 using Microsoft.ApplicationInsights.Extensibility;
+using Microsoft.ApplicationInsights.Extensibility.Implementation;
+using Moq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Xunit;
 
 namespace NuGet.Services.Logging.Tests
 {
     public class TelemetryResponseCodeProcessorFacts
     {
-        [Theory]
-        [InlineData(new int[] { 401 }, "401")]
-        [InlineData(new int[] { 400, 404 }, "400")]
-        [InlineData(new int[] { 400, 404 }, "404")]
-        public void MarksOverridenResponseCodesAsSuccessful(int[] responseCodeOverrides, string responseCode)
+        [Fact]
+        public void Constructor_ThrowsForNullNext()
         {
-            // Arrange
-            var failed = new RequestTelemetry
-            {
-                ResponseCode = responseCode,
-                Success = false,
-            };
+            var exception = Assert.Throws<ArgumentNullException>(() => new TelemetryResponseCodeProcessor(next: null));
 
-            var successful = new RequestTelemetry
-            {
-                ResponseCode = responseCode,
-                Success = true,
-            };
+            Assert.Equal("next", exception.ParamName);
+        }
 
-            // Assert
-            Assert.True(ProcessResponseCode(responseCodeOverrides, failed).Success);
-            Assert.True(ProcessResponseCode(responseCodeOverrides, successful).Success);
+        [Fact]
+        public void SuccessfulResponseCodes_IsEmptyByDefault()
+        {
+            var filter = new TelemetryResponseCodeProcessor(Mock.Of<ITelemetryProcessor>());
+
+            Assert.Empty(filter.SuccessfulResponseCodes);
+        }
+
+        [Fact]
+        public void Process_ThrowsForNullItem()
+        {
+            var filter = new TelemetryResponseCodeProcessor(Mock.Of<ITelemetryProcessor>());
+
+            var exception = Assert.Throws<ArgumentNullException>(() => filter.Process(item: null));
+
+            Assert.Equal("item", exception.ParamName);
         }
 
         [Theory]
-        [InlineData(new int[] { 200 }, "400")]
-        [InlineData(new int[] { 400, 404 }, "200")]
-        [InlineData(new int[] { 400, 404 }, "301")]
-        [InlineData(new int[] { 400, 404 }, "401")]
-        [InlineData(new int[] { 400, 404 }, "403")]
-        [InlineData(new int[] { 400, 404 }, "410")]
-        [InlineData(new int[] { 400, 404 }, "500")]
-        public void DoesntAffectOtherResponses(int[] responseCodeOverrides, string responseCode)
+        [InlineData(true)]
+        [InlineData(false)]
+        public void Process_SetsSuccessToTrueForRequestTelemetryWithSuccessfulResponseCode(bool actualSuccess)
         {
-            // Arrange
-            var failed = new RequestTelemetry
+            var responseCode = 400;
+            var telemetry = new RequestTelemetry()
             {
-                ResponseCode = responseCode,
-                Success = false,
+                ResponseCode = responseCode.ToString(),
+                Success = actualSuccess
             };
+            var filter = new TelemetryResponseCodeProcessor(Mock.Of<ITelemetryProcessor>());
 
-            var successful = new RequestTelemetry
-            {
-                ResponseCode = responseCode,
-                Success = true,
-            };
+            filter.SuccessfulResponseCodes.Add(responseCode);
+            filter.Process(telemetry);
 
-            // Assert
-            Assert.False(ProcessResponseCode(responseCodeOverrides, failed).Success);
-            Assert.True(ProcessResponseCode(responseCodeOverrides, successful).Success);
+            Assert.True(telemetry.Success);
         }
 
-        private class TelemetryCallbackProcessor : ITelemetryProcessor
+        [Theory]
+        [InlineData(400)]
+        [InlineData(404)]
+        public void Process_MultipleSuccessfulResponseCodes_SetsSuccessToTrueForRequestTelemetryWithSuccessfulResponseCode(
+            int responseCode)
         {
-            private Action<ITelemetry> _callback;
-
-            public TelemetryCallbackProcessor(Action<ITelemetry> callback)
+            var telemetry = new RequestTelemetry()
             {
-                _callback = callback;
-            }
+                ResponseCode = responseCode.ToString(),
+                Success = false
+            };
+            var filter = new TelemetryResponseCodeProcessor(Mock.Of<ITelemetryProcessor>());
 
-            public void Process(ITelemetry item)
-            {
-                _callback(item);
-            }
+            filter.SuccessfulResponseCodes.Add(400);
+            filter.SuccessfulResponseCodes.Add(404);
+
+            filter.Process(telemetry);
+
+            Assert.True(telemetry.Success);
         }
 
-        private RequestTelemetry ProcessResponseCode(int[] successCodeOverrides, RequestTelemetry telemetry)
+        [Theory]
+        [InlineData(null, true)]
+        [InlineData(null, false)]
+        [InlineData("", true)]
+        [InlineData("", false)]
+        [InlineData("a", true)]
+        [InlineData("a", false)]
+        [InlineData("-1", true)]
+        [InlineData("-1", false)]
+        [InlineData("1.2", true)]
+        [InlineData("1.2", false)]
+        [InlineData("401", true)]
+        [InlineData("401", false)]
+        public void Process_DoesNotModifySuccessForRequestTelemetryWithOtherResponseCodes(
+            string responseCode,
+            bool actualSuccess)
         {
-            ITelemetry result = null;
-            var processor = new TelemetryResponseCodeProcessor(new TelemetryCallbackProcessor(i => result = i));
-
-            foreach (var successCode in successCodeOverrides)
+            var telemetry = new RequestTelemetry()
             {
-                processor.SuccessfulResponseCodes.Add(successCode);
-            }
+                ResponseCode = responseCode,
+                Success = actualSuccess
+            };
+            var filter = new TelemetryResponseCodeProcessor(Mock.Of<ITelemetryProcessor>());
 
-            processor.Process(telemetry);
+            filter.SuccessfulResponseCodes.Add(400);
+            filter.Process(telemetry);
 
-            return result as RequestTelemetry;
+            Assert.Equal(actualSuccess, telemetry.Success);
+        }
+
+        [Theory]
+        [InlineData(400)]
+        [InlineData(401)]
+        public void Process_AlwaysCallsNextProcessorForRequestTelemetry(int responseCode)
+        {
+            var telemetry = new RequestTelemetry()
+            {
+                ResponseCode = responseCode.ToString(),
+                Success = false
+            };
+            var nextProcessor = new Mock<ITelemetryProcessor>(MockBehavior.Strict);
+
+            nextProcessor.Setup(x => x.Process(It.Is<RequestTelemetry>(rt => ReferenceEquals(rt, telemetry))));
+
+            var filter = new TelemetryResponseCodeProcessor(nextProcessor.Object);
+
+            filter.SuccessfulResponseCodes.Add(400);
+            filter.Process(telemetry);
+
+            nextProcessor.Verify(
+                x => x.Process(It.Is<RequestTelemetry>(rt => ReferenceEquals(rt, telemetry))),
+                Times.Once());
+        }
+
+        [Fact]
+        public void Process_CallsNextProcessorForExceptionTelemetryWithNullException()
+        {
+            var telemetry = new ExceptionTelemetry();
+
+            // By default this property is null.
+            // However, assert this fact anyway to guard against an unexpected change in the dependency
+            // which would invalidate the setup for this test.
+            Assert.Null(telemetry.Exception);
+
+            var nextProcessor = new Mock<ITelemetryProcessor>(MockBehavior.Strict);
+
+            nextProcessor.Setup(x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))));
+
+            var filter = new TelemetryResponseCodeProcessor(nextProcessor.Object);
+
+            filter.Process(telemetry);
+
+            nextProcessor.Verify(
+                x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))),
+                Times.Once());
+        }
+
+        [Fact]
+        public void Process_CallsNextProcessorForExceptionTelemetryWithExceptionThatIsNotHttpException()
+        {
+            var telemetry = new ExceptionTelemetry(new NullReferenceException());
+            var nextProcessor = new Mock<ITelemetryProcessor>(MockBehavior.Strict);
+
+            nextProcessor.Setup(x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))));
+
+            var filter = new TelemetryResponseCodeProcessor(nextProcessor.Object);
+
+            filter.Process(telemetry);
+
+            nextProcessor.Verify(
+                x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))),
+                Times.Once());
+        }
+
+        [Theory]
+        [InlineData(500)]
+        [InlineData(503)]
+        public void Process_CallsNextProcessorForExceptionTelemetryWithHttpStatusCodeGreaterThanOrEqualTo500(
+            int statusCode)
+        {
+            var telemetry = new ExceptionTelemetry(new HttpException(statusCode, message: "a"));
+            var nextProcessor = new Mock<ITelemetryProcessor>(MockBehavior.Strict);
+
+            nextProcessor.Setup(x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))));
+
+            var filter = new TelemetryResponseCodeProcessor(nextProcessor.Object);
+
+            filter.Process(telemetry);
+
+            nextProcessor.Verify(
+                x => x.Process(It.Is<ExceptionTelemetry>(rt => ReferenceEquals(rt, telemetry))),
+                Times.Once());
+        }
+
+        [Fact]
+        public void Process_CallsNextProcessorWithTraceTelemetryForExceptionTelemetryWithHttpStatusCodeLessThan500()
+        {
+            var telemetry = new ExceptionTelemetry(new HttpException(httpCode: 499, message: "a"));
+            var nextProcessor = new Mock<ITelemetryProcessor>(MockBehavior.Strict);
+
+            nextProcessor.Setup(x => x.Process(It.IsNotNull<TraceTelemetry>()));
+
+            var filter = new TelemetryResponseCodeProcessor(nextProcessor.Object);
+
+            filter.Process(telemetry);
+
+            nextProcessor.Verify(x => x.Process(It.IsNotNull<TraceTelemetry>()), Times.Once());
+        }
+
+        [Fact]
+        public void Process_CallsNextProcessorWithTraceTelemetryPopulatedFromExceptionTelemetry()
+        {
+            var exception = new HttpException(httpCode: 401, message: "a");
+            var exceptionJson = JObject.FromObject(exception).ToString(Formatting.None);
+            var timestamp = DateTimeOffset.UtcNow;
+            var exceptionTelemetry = new ExceptionTelemetry(exception);
+
+            exceptionTelemetry.Context.Cloud.RoleInstance = "b";
+            exceptionTelemetry.Context.Cloud.RoleName = "c";
+            exceptionTelemetry.Context.GetInternalContext().SdkVersion = "d";
+            exceptionTelemetry.Context.InstrumentationKey = "e";
+            exceptionTelemetry.Context.Operation.Id = "f";
+            exceptionTelemetry.Context.Operation.Name = "g";
+            exceptionTelemetry.Context.Operation.ParentId = "h";
+            exceptionTelemetry.Context.Location.Ip = "127.0.0.1";
+            exceptionTelemetry.Properties.Add("i", "j");
+            exceptionTelemetry.Sequence = "k";
+            exceptionTelemetry.Timestamp = timestamp;
+
+            var nextProcessor = new Mock<ITelemetryProcessor>(MockBehavior.Strict);
+
+            nextProcessor.Setup(x => x.Process(It.Is<TraceTelemetry>(
+                    t => t.Message == "a" &&
+                        t.SeverityLevel == SeverityLevel.Warning &&
+                        t.Context.Cloud.RoleInstance == "b" &&
+                        t.Context.Cloud.RoleName == "c" &&
+                        t.Context.GetInternalContext().SdkVersion == "d" &&
+                        t.Context.InstrumentationKey == "e" &&
+                        t.Context.Operation.Id == "f" &&
+                        t.Context.Operation.Name == "g" &&
+                        t.Context.Operation.ParentId == "h" &&
+                        t.Context.Location.Ip == "127.0.0.1" &&
+                        t.Properties["i"] == "j" &&
+                        t.Properties["exception"] == exceptionJson &&
+                        t.Sequence == "k" &&
+                        t.Timestamp == timestamp)))
+                .Verifiable();
+
+            var filter = new TelemetryResponseCodeProcessor(nextProcessor.Object);
+
+            filter.Process(exceptionTelemetry);
+
+            nextProcessor.Verify();
         }
     }
 }
