4b54f4c66ce61c5b8c8c8e455cddc2437ff96391
Loïc Sharma <sharma.loic@gmail.com>
2017-10-03 17:17:17 -0700

    Added context interface; added ValidatorName property to ValidatorStatuses table

---
+++ src/NuGet.Services.Validation/Entities/IValidationContext.cs
@@ -0,0 +1,24 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure.Annotations;
+using System.Threading.Tasks;
+
+namespace NuGet.Services.Validation
+{
+    public interface IValidationEntitiesContext
+    {
+        IDbSet<PackageValidationSet> PackageValidationSets { get; set; }
+        IDbSet<PackageValidation> PackageValidations { get; set; }
+        IDbSet<ValidatorStatus> ValidatorStatuses { get; set; }
+
+        IDbSet<PackageSigningState> PackageSigningStates { get; set; }
+        IDbSet<PackageSignature> PackageSignatures { get; set; }
+        IDbSet<Certificate> Certificates { get; set; }
+        IDbSet<CertificateValidation> CertificateValidations { get; set; }
+
+        Task<int> SaveChangesAsync();
+    }
+}
--- src/NuGet.Services.Validation/Migrations/201710040015565_AddPackageSigningSchema.cs
+++ src/NuGet.Services.Validation/Migrations/201710040015565_AddPackageSigningSchema.cs
@@ -70,6 +70,7 @@ namespace NuGet.Services.Validation
                     {
                         ValidationId = c.Guid(nullable: false),
                         PackageKey = c.Int(nullable: false),
+                        ValidatorName = c.String(),
                         State = c.Int(nullable: false),
                     })
                 .PrimaryKey(t => t.ValidationId)
