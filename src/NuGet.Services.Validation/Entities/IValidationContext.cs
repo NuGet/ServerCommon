cc625ce7711d13f39140d7967298b9c6ff927eb8
Loïc Sharma <sharma.loic@gmail.com>
2017-10-03 18:00:11 -0700

    Purging set;. Muahaha

--- src/NuGet.Services.Validation/Entities/IValidationContext.cs
+++ src/NuGet.Services.Validation/Entities/IValidationContext.cs
@@ -1,23 +1,21 @@
 ﻿// Copyright (c) .NET Foundation. All rights reserved.
 // Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
 
-using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
-using System.Data.Entity.Infrastructure.Annotations;
 using System.Threading.Tasks;
 
 namespace NuGet.Services.Validation
 {
     public interface IValidationEntitiesContext
     {
-        IDbSet<PackageValidationSet> PackageValidationSets { get; set; }
-        IDbSet<PackageValidation> PackageValidations { get; set; }
-        IDbSet<ValidatorStatus> ValidatorStatuses { get; set; }
+        IDbSet<PackageValidationSet> PackageValidationSets { get; }
+        IDbSet<PackageValidation> PackageValidations { get; }
+        IDbSet<ValidatorStatus> ValidatorStatuses { get; }
 
-        IDbSet<PackageSigningState> PackageSigningStates { get; set; }
-        IDbSet<PackageSignature> PackageSignatures { get; set; }
-        IDbSet<Certificate> Certificates { get; set; }
-        IDbSet<CertificateValidation> CertificateValidations { get; set; }
+        IDbSet<PackageSigningState> PackageSigningStates { get; }
+        IDbSet<PackageSignature> PackageSignatures { get; }
+        IDbSet<Certificate> Certificates { get; }
+        IDbSet<CertificateValidation> CertificateValidations { get; }
 
         Task<int> SaveChangesAsync();
     }
