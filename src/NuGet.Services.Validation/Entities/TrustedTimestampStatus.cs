96bd580ba9dc0c8860af651ce68f3af5344072f2
Loïc Sharma <sharma.loic@gmail.com>
2018-02-05 13:00:45 -0800

    Added Status column to TrustedTimestamp entity

---
+++ src/NuGet.Services.Validation/Entities/TrustedTimestampStatus.cs
@@ -0,0 +1,22 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+namespace NuGet.Services.Validation
+{
+    /// <summary>
+    /// The status for a given <see cref="EndCertificate"/>.
+    /// </summary>
+    public enum TrustedTimestampStatus
+    {
+        /// <summary>
+        /// The <see cref="TrustedTimestamp" /> has failed validation. This could be for a number of reasons including
+        /// an invalid or revoked certificate. The timestamp should no longer be trusted.
+        /// </summary>
+        Invalid = 0,
+
+        /// <summary>
+        /// The <see cref="TrustedTimestamp" /> is valid and trusted.
+        /// </summary>
+        Valid = 1,
+    }
+}
