f20b2faad7ed6007c16967ccb7e18ce4fa05280f
Christy Henriksson <chenriks@microsoft.com>
2018-07-18 07:39:23 -0700

    fix

--- src/NuGet.Services.Sql/ISqlConnectionFactory.cs
+++ src/NuGet.Services.Sql/ISqlConnectionFactory.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace NuGet.Services.Sql
 {
-    internal interface ISqlConnectionFactory
+    public interface ISqlConnectionFactory
     {
         string ApplicationName { get; }
 
