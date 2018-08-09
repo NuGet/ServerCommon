732ae055e586a36896c29f694c4664a0c201e590
Scott Bommarito <scottbom@microsoft.com>
2018-08-08 17:17:20 -0700

    named cursors

--- src/NuGet.Services.Status.Table/CursorEntity.cs
+++ src/NuGet.Services.Status.Table/CursorEntity.cs
@@ -9,17 +9,19 @@ namespace NuGet.Services.Status.Table
     public class CursorEntity : TableEntity
     {
         public const string DefaultPartitionKey = "cursors";
-        public const string DefaultRowKey = "1";
 
         public CursorEntity()
         {
         }
 
-        public CursorEntity(DateTime value)
-            : base(DefaultPartitionKey, DefaultRowKey)
+        public CursorEntity(string name, DateTime value)
+            : base(DefaultPartitionKey, name)
         {
+            Name = name;
             Value = value;
         }
+
+        public string Name { get; set; }
 
         public DateTime Value { get; set; }
     }
