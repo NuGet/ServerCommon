4de0c08fc05c65b2bdc2df4688de6ecf16e1780e
Xavier Decoster <xadeco@microsoft.com>
2018-10-19 06:37:19 -0700

    Ported package extension method from gallery dev branch

--- src/NuGet.Services.Entities/Extensions/PackageExtensions.cs
+++ src/NuGet.Services.Entities/Extensions/PackageExtensions.cs
@@ -19,5 +19,16 @@ namespace NuGet.Services.Entities
                 .OrderByDescending(sp => sp.Created)
                 .FirstOrDefault();
         }
+
+        /// <summary>
+        /// Returns true if there exists a symbol package which is latest and is in
+        /// available state, false otherwise.
+        /// </summary>
+        public static bool IsLatestSymbolPackageAvailable(this Package package)
+        {
+            var latestSymbolPackage = package.LatestSymbolPackage();
+            return latestSymbolPackage != null
+                && latestSymbolPackage.StatusKey == PackageStatus.Available;
+        }
     }
 }
