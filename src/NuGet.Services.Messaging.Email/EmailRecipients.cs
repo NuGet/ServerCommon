213b16aaf8886cd7ae0a500eab0c3a5250566cb2
Scott Bommarito <scottbom@microsoft.com>
2018-11-07 16:01:16 -0800

    add back emailrecipients none

--- src/NuGet.Services.Messaging.Email/EmailRecipients.cs
+++ src/NuGet.Services.Messaging.Email/EmailRecipients.cs
@@ -23,6 +23,8 @@ namespace NuGet.Services.Messaging.Email
             ReplyTo = replyTo ?? new List<MailAddress>();
         }
 
+        public static IEmailRecipients None = new EmailRecipients(to: Array.Empty<MailAddress>());
+
         public IReadOnlyList<MailAddress> To { get; }
 
         public IReadOnlyList<MailAddress> CC { get; }
