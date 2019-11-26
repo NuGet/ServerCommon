40cf03944c4763fd0457fbeb07ea0cfdb27fe0f4
Xavier Decoster <xavierdecoster@users.noreply.github.com>
2019-11-26 12:30:33 +0100

    Avoid static state. Added comments. Make DiagnosticsTelemetryModule accessible to allow tweaking heartbeats.

--- src/NuGet.Services.Logging/ApplicationInsights.cs
+++ src/NuGet.Services.Logging/ApplicationInsights.cs
@@ -9,69 +9,85 @@ using Microsoft.ApplicationInsights.Extensibility.Implementation.Tracing;
 
 namespace NuGet.Services.Logging
 {
+    /// <summary>
+    /// Utility class to initialize an <see cref="ApplicationInsightsConfiguration"/> instance
+    /// using provided instrumentation key, optional heartbeat interval, 
+    /// and, if detected, taking into account an optional ApplicationInsights.config file.
+    /// </summary>
+    /// <remarks>
+    /// Calling <see cref="Initialize(string)"/> or <see cref="Initialize(string, TimeSpan)"/> returns the
+    /// initialized <see cref="ApplicationInsightsConfiguration"/> object; 
+    /// it does not set the obsolete <see cref="TelemetryClient.Active"/> component.
+    /// 
+    /// It is the caller's responsibility to ensure the returned configuration is used 
+    /// when creating new <see cref="TelemetryClient"/> instances.
+    /// </remarks>
     public static class ApplicationInsights
     {
-        public static IHeartbeatPropertyManager HeartbeatManager { get; private set; }
-
-        public static bool Initialized { get; private set; }
-
-        public static TelemetryConfiguration Initialize(string instrumentationKey)
+        /// <summary>
+        /// Initializes an <see cref="ApplicationInsightsConfiguration"/> using the provided
+        /// <paramref name="instrumentationKey"/>, taking into account the <c>ApplicationInsights.config</c> file if present.
+        /// </summary>
+        /// <param name="instrumentationKey">The instrumentation key to use.</param>
+        public static ApplicationInsightsConfiguration Initialize(string instrumentationKey)
         {
-            return InitializeTelemetryConfiguration(instrumentationKey, heartbeatInterval: null);
+            return InitializeApplicationInsightsConfiguration(instrumentationKey, heartbeatInterval: null);
         }
 
-        public static TelemetryConfiguration Initialize(string instrumentationKey, TimeSpan heartbeatInterval)
+        /// <summary>
+        /// Initializes an <see cref="ApplicationInsightsConfiguration"/> using the provided
+        /// <paramref name="instrumentationKey"/> and <paramref name="heartbeatInterval"/>, 
+        /// taking into account the <c>ApplicationInsights.config</c> file if present.
+        /// </summary>
+        /// <param name="instrumentationKey">The instrumentation key to use.</param>
+        /// <param name="heartbeatInterval">The heartbeat interval to use.</param>
+        public static ApplicationInsightsConfiguration Initialize(
+            string instrumentationKey,
+            TimeSpan heartbeatInterval)
         {
-            return InitializeTelemetryConfiguration(instrumentationKey, heartbeatInterval);
+            return InitializeApplicationInsightsConfiguration(instrumentationKey, heartbeatInterval);
         }
 
-        private static TelemetryConfiguration InitializeTelemetryConfiguration(string instrumentationKey, TimeSpan? heartbeatInterval)
+        private static ApplicationInsightsConfiguration InitializeApplicationInsightsConfiguration(
+            string instrumentationKey,
+            TimeSpan? heartbeatInterval)
         {
-            TelemetryConfiguration telemetryConfiguration = null;
+            // Note: TelemetryConfiguration.Active is being deprecated
+            // https://github.com/microsoft/ApplicationInsights-dotnet/issues/1152
+            // We use TelemetryConfiguration.CreateDefault() as opposed to instantiating a new TelemetryConfiguration()
+            // to take into account the ApplicationInsights.config file (if detected).
+            var telemetryConfiguration = TelemetryConfiguration.CreateDefault();
 
             if (!string.IsNullOrWhiteSpace(instrumentationKey))
             {
-                // Note: TelemetryConfiguration.Active is being deprecated
-                // https://github.com/microsoft/ApplicationInsights-dotnet/issues/1152
-
-                telemetryConfiguration = TelemetryConfiguration.CreateDefault();
                 telemetryConfiguration.InstrumentationKey = instrumentationKey;
-                telemetryConfiguration.TelemetryInitializers.Add(new TelemetryContextInitializer());
+            }
 
-                // Construct a TelemetryClient to emit traces so we can track and debug AI initialization.
-                var telemetryClient = new TelemetryClient(telemetryConfiguration);
+            telemetryConfiguration.TelemetryInitializers.Add(new TelemetryContextInitializer());
 
-                telemetryClient.TrackTrace(
-                            $"TelemetryConfiguration initialized using instrumentation key: {instrumentationKey}.",
-                            SeverityLevel.Information);
+            // Construct a TelemetryClient to emit traces so we can track and debug AI initialization.
+            var telemetryClient = new TelemetryClient(telemetryConfiguration);
 
-                // Configure heartbeat interval if specified.
-                // When not defined or null, the DiagnosticsTelemetryModule will use its internal defaults (heartbeat enabled, interval of 15 minutes).
-                if (heartbeatInterval.HasValue)
-                {
-                    var diagnosticsTelemetryModule = new DiagnosticsTelemetryModule();
-                    diagnosticsTelemetryModule.HeartbeatInterval = heartbeatInterval.Value;
-                    diagnosticsTelemetryModule.Initialize(telemetryConfiguration);
-
-                    telemetryClient.TrackTrace(
-                            $"DiagnosticsTelemetryModule initialized using configured heartbeat interval: {heartbeatInterval.Value}.",
-                            SeverityLevel.Information);
-                }
-                else
-                {
-                    telemetryClient.TrackTrace(
-                        "Telemetry initialized using default heartbeat interval.",
+            telemetryClient.TrackTrace(
+                        $"TelemetryConfiguration initialized using instrumentation key: {instrumentationKey ?? "EMPTY"}.",
                         SeverityLevel.Information);
-                }
 
-                Initialized = true;
-            }
-            else
+            var diagnosticsTelemetryModule = new DiagnosticsTelemetryModule();
+
+            // Configure heartbeat interval if specified.
+            // When not defined, the DiagnosticsTelemetryModule will use its internal defaults (heartbeat enabled, interval of 15 minutes).
+            var traceMessage = "DiagnosticsTelemetryModule initialized using default heartbeat interval.";
+            if (heartbeatInterval.HasValue)
             {
-                Initialized = false;
+                diagnosticsTelemetryModule.HeartbeatInterval = heartbeatInterval.Value;
+                traceMessage = $"DiagnosticsTelemetryModule initialized using configured heartbeat interval: {heartbeatInterval.Value}.";
             }
 
-            return telemetryConfiguration;
+            diagnosticsTelemetryModule.Initialize(telemetryConfiguration);
+
+            telemetryClient.TrackTrace(traceMessage, SeverityLevel.Information);
+
+            return new ApplicationInsightsConfiguration(telemetryConfiguration, diagnosticsTelemetryModule);
         }
     }
 }
