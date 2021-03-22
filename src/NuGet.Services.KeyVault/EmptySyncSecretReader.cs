e4302c155b611d0b3fcb1e531152cde15364cf6b
Andrei Grigorev <angrigor@microsoft.com>
2021-03-22 13:47:05 -0700

    EmptySyncSecretReader

---
+++ src/NuGet.Services.KeyVault/EmptySyncSecretReader.cs
@@ -0,0 +1,17 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+using System.Threading.Tasks;
+using Microsoft.Extensions.Logging;
+
+namespace NuGet.Services.KeyVault
+{
+    public class EmptySyncSecretReader : ISyncSecretReader
+    {
+        public string GetSecret(string secretName)
+            => secretName;
+
+        public ISecret GetSecretObject(string secretName)
+            => new KeyVaultSecret(secretName, secretName, null);
+    }
+}
