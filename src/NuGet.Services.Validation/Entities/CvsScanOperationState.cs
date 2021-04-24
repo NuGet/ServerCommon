48ac0f99aaa4211b00a7b40b1d282b76e81343aa
Anup Kumar <anukumar@microsoft.com>
2021-04-24 15:14:52 +0530

    CvsScanOperation

---
+++ src/NuGet.Services.Validation/Entities/CvsScanOperationState.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NuGet.Services.Validation.Entities
+{
+    /// <summary>
+    /// Status of the cvs scan operations
+    /// </summary>
+    public class CvsScanOperationState
+    {
+
+        /// <summary>
+        /// The database-mastered identifier for this operation.
+        /// </summary>
+        public long Key { get; set; }
+
+        /// <summary>
+        /// Validation ID for which this operation is performed.
+        /// </summary>
+        public Guid ValidationSetId { get; set; }
+
+        /// <summary>
+        /// Status of the operation performed.
+        /// </summary>
+        public int Status { get; set; }
+
+        /// <summary>
+        /// Job Id of the operation.
+        /// </summary>
+        public long JobId { get; set; }
+
+        /// <summary>
+        /// Time when the validator detected operation request.
+        /// </summary>
+        public DateTime CreatedAt { get; set; }
+
+        /// <summary>
+        /// Time when the operation updated.
+        /// </summary>
+        public DateTime? LastUpdatedAt { get; set; }
+
+        /// <summary>
+        /// The size of the blob sent for scan/sign operation
+        /// </summary>
+        public string? Violation { get; set; }
+
+        /// <summary>
+        /// The path pointing to external content
+        /// </summary>
+        public string ContentPath { get; set; }
+
+        /// <summary>
+        /// The url pointing to an operation output blob.
+        /// </summary>
+        public string BlobUrl { get; set; }
+
+        /// <summary>
+        /// File ID
+        /// </summary>
+        public string FileId { get; set; }
+
+        /// <summary>
+        /// Row Version
+        /// </summary>
+        public byte[] RowVersion { get; set; }
+    }
+}
