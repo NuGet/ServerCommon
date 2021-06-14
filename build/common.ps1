947491f5608c02853234827a5c3cee7dc615c013
Daniel Jacinto <dajacint@microsoft.com>
2021-06-14 16:24:37 -0600

    Remove endpoints URI

--- build/common.ps1
+++ build/common.ps1
@@ -475,20 +475,16 @@ Function Install-NuGet {
 
     if ($AccessToken) {
         $endpoints = @()
-        $endpointURIs = @()
 
         Get-ChildItem "$NuGetClientRoot\nuget.config" -Recurse |% {
             $nugetConfig = [xml](Get-Content -Path $_)
 
             $nugetConfig.configuration.packageSources.add |? { ($_.value -match '^https://pkgs\.dev\.azure\.com/') -or ($_.value -match '^https://[\w\-]+\.pkgs\.visualstudio\.com/') } |% {
-                if ($endpointURIs -notcontains $_.Value) {
-                    $endpointURIs += $_.Value;
-                    $endpoint = New-Object -TypeName PSObject;
-                    Add-Member -InputObject $endpoint -MemberType NoteProperty -Name endpoint -Value $_.value;
-                    Add-Member -InputObject $endpoint -MemberType NoteProperty -Name username -Value 'PAT';
-                    Add-Member -InputObject $endpoint -MemberType NoteProperty -Name password -Value $AccessToken;
-                    $endpoints += $endpoint;
-                }
+                $endpoint = New-Object -TypeName PSObject;
+                Add-Member -InputObject $endpoint -MemberType NoteProperty -Name endpoint -Value $_.value;
+                Add-Member -InputObject $endpoint -MemberType NoteProperty -Name username -Value 'PAT';
+                Add-Member -InputObject $endpoint -MemberType NoteProperty -Name password -Value $AccessToken;
+                $endpoints += $endpoint;
             }
         }
 
