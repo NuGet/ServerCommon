f8832fcaad2569b9350931b5ae3b3faf27346e10
Joel Verhagen <jver@microsoft.com>
2022-07-14 10:06:28 -0700

    Default to newer VS

--- build/common.ps1
+++ build/common.ps1
@@ -1,5 +1,5 @@
 ### Constants ###
-$DefaultMSBuildVersion = '16'
+$DefaultMSBuildVersion = '17'
 $DefaultConfiguration = 'debug'
 $NuGetClientRoot = Split-Path -Path $PSScriptRoot -Parent
 $CLIRoot = Join-Path $NuGetClientRoot 'cli'
