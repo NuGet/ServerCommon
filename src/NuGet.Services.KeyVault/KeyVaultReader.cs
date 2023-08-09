8606cd86757eb95c4c5039e21781c7815084a83e
Andrei Grigorev <angrigor@microsoft.com>
2023-08-08 18:18:12 -0700

    Putting DefaultAzureCredential back.

--- src/NuGet.Services.KeyVault/KeyVaultReader.cs
+++ src/NuGet.Services.KeyVault/KeyVaultReader.cs
@@ -61,7 +61,7 @@ namespace NuGet.Services.KeyVault
             TokenCredential credential = null;
             if (_configuration.UseManagedIdentity)
             {
-                credential = new ManagedIdentityCredential();
+                credential = new DefaultAzureCredential();
             }
             else
             {
