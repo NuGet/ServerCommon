222f60246c0935d823daf14527826ad3c27b91ec
Peter Yu <ryuyu@microsoft.com>
2024-05-29 18:04:06 -0700

    Temporarily disable emu tests and add new interface ones.

--- tests/NuGet.Services.Storage.Tests/AzureStorageFacts.cs
+++ tests/NuGet.Services.Storage.Tests/AzureStorageFacts.cs
@@ -6,8 +6,10 @@ using System.IO;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using Azure.Storage.DataMovement.Blobs;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Models;
 using Microsoft.Extensions.Logging;
-using Microsoft.WindowsAzure.Storage;
 using Moq;
 using Xunit;
 
@@ -15,116 +17,117 @@ namespace NuGet.Services.Storage.Tests
 {
     public class AzureStorageFacts
     {
-        private const string NoEmulator = "The Azure Storage emulator is not running on the CI.";
-        private const string ContainerName = "test";
-        private static readonly Uri BaseAddress = new Uri("http://example/dir/");
-        private const string BlobName = "blob.txt";
-        private const string InitialContent = "pre-existing content";
+        //private const string NoEmulator = "The Azure Storage emulator is not running on the CI.";
+        //private const string ContainerName = "test";
+        //private static readonly Uri BaseAddress = new Uri("http://example/dir/");
+        //private const string BlobName = "blob.txt";
+        //private const string InitialContent = "pre-existing content";
 
-        public class Load : BaseFacts
-        {
-            [Theory(Skip = NoEmulator)]
-            [InlineData(true)]
-            [InlineData(false)]
-            public async Task ProperlyRoundTripsBytes(bool compressContent)
-            {
-                // Arrange
-                await CreateBlobAsync(_dirPath, BlobName);
+        //public class Load : BaseFacts
+        //{
+        //    [Theory(Skip = NoEmulator)]
+        //    [InlineData(true)]
+        //    [InlineData(false)]
+        //    public async Task ProperlyRoundTripsBytes(bool compressContent)
+        //    {
+        //        // Arrange
+        //        await CreateBlobAsync(_dirPath, BlobName);
 
-                _target.CompressContent = compressContent;
+        //        _target.CompressContent = compressContent;
 
-                var inputBytes = new byte[] { 0x80 };
-                var inputContent = new StreamStorageContent(new MemoryStream(inputBytes));
+        //        var inputBytes = new byte[] { 0x80 };
+        //        var inputContent = new StreamStorageContent(new MemoryStream(inputBytes));
 
-                await _target.Save(_resourceUri, inputContent, overwrite: true, cancellationToken: CancellationToken.None);
+        //        await _target.Save(_resourceUri, inputContent, overwrite: true, cancellationToken: CancellationToken.None);
 
-                // Act
-                var response = await _target.Load(_resourceUri, CancellationToken.None);
+        //        // Act
+        //        var response = await _target.Load(_resourceUri, CancellationToken.None);
 
-                // Assert
-                using (var stream = response.GetContentStream())
-                {
-                    var buffer = new MemoryStream();
-                    await stream.CopyToAsync(buffer);
-                    var outputBytes = buffer.ToArray();
-                    Assert.Equal(inputBytes, outputBytes);
-                }
-            }
-        }
+        //        // Assert
+        //        using (var stream = response.GetContentStream())
+        //        {
+        //            var buffer = new MemoryStream();
+        //            await stream.CopyToAsync(buffer);
+        //            var outputBytes = buffer.ToArray();
+        //            Assert.Equal(inputBytes, outputBytes);
+        //        }
+        //    }
+        //}
 
-        public class Save : BaseFacts
-        {
-            [Fact(Skip = NoEmulator)]
-            public async Task ThrowsWhenOverwritesAreNotDesired()
-            {
-                // Arrange
-                await CreateBlobAsync(_dirPath, BlobName);
+        //public class Save : BaseFacts
+        //{
+        //    [Fact(Skip = NoEmulator)]
+        //    public async Task ThrowsWhenOverwritesAreNotDesired()
+        //    {
+        //        // Arrange
+        //        await CreateBlobAsync(_dirPath, BlobName);
 
-                var content = new StringStorageContent("new content");
+        //        var content = new StringStorageContent("new content");
 
-                // Act & Assert
-                var exception = await Assert.ThrowsAsync<Exception>(
-                    () => _target.Save(_resourceUri, content, overwrite: false, cancellationToken: CancellationToken.None));
+        //        // Act & Assert
+        //        var exception = await Assert.ThrowsAsync<Exception>(
+        //            () => _target.Save(_resourceUri, content, overwrite: false, cancellationToken: CancellationToken.None));
 
-                Assert.Contains("The remote server returned an error: (409) Conflict.", exception.Message);
+        //        Assert.Contains("The remote server returned an error: (409) Conflict.", exception.Message);
 
-                var currentContent = await _target.LoadString(_resourceUri, CancellationToken.None);
-                Assert.Equal(InitialContent, currentContent);
-            }
+        //        var currentContent = await _target.LoadString(_resourceUri, CancellationToken.None);
+        //        Assert.Equal(InitialContent, currentContent);
+        //    }
 
-            [Fact(Skip = NoEmulator)]
-            public async Task AllowsOverwriteWhenDesired()
-            {
-                // Arrange
-                await CreateBlobAsync(_dirPath, BlobName);
+        //    [Fact(Skip = NoEmulator)]
+        //    public async Task AllowsOverwriteWhenDesired()
+        //    {
+        //        // Arrange
+        //        await CreateBlobAsync(_dirPath, BlobName);
 
-                var inputContent = new StringStorageContent("new content");
+        //        var inputContent = new StringStorageContent("new content");
 
-                // Act
-                await _target.Save(_resourceUri, inputContent, overwrite: true, cancellationToken: CancellationToken.None);
+        //        // Act
+        //        await _target.Save(_resourceUri, inputContent, overwrite: true, cancellationToken: CancellationToken.None);
                 
-                // Assert
-                var currentContent = await _target.LoadString(_resourceUri, CancellationToken.None);
-                Assert.Equal(inputContent.Content, currentContent);
-            }
-        }
+        //        // Assert
+        //        var currentContent = await _target.LoadString(_resourceUri, CancellationToken.None);
+        //        Assert.Equal(inputContent.Content, currentContent);
+        //    }
+        //}
 
-        public class BaseFacts
-        {
-            protected readonly string _dirPath;
-            protected readonly AzureStorage _target;
-            protected readonly Uri _resourceUri;
+        //public class BaseFacts
+        //{
+        //    protected readonly string _dirPath;
+        //    protected readonly AzureStorage _target;
+        //    protected readonly Uri _resourceUri;
 
-            public BaseFacts()
-            {
-                _dirPath = Guid.NewGuid().ToString();
-                _target = new AzureStorage(
-                    CloudStorageAccount.DevelopmentStorageAccount,
-                    ContainerName,
-                    _dirPath,
-                    BaseAddress,
-                    useServerSideCopy: true,
-                    initializeContainer: true,
-                    logger: new Mock<ILogger<AzureStorage>>().Object);
+        //    public BaseFacts()
+        //    {
+        //        _dirPath = Guid.NewGuid().ToString();
+        //        _target = new AzureStorage(
+        //            new BlobServiceClient(),
+        //            new BlobsStorageResourceProvider(),
+        //            containerName: _dirPath,
+        //            path: _dirPath,
+        //            baseAddress: BaseAddress,
+        //            useServerSideCopy: true,
+        //            initializeContainer: true,
+        //            logger: new Mock<ILogger<AzureStorage>>().Object);
 
-                _resourceUri = new Uri(BaseAddress, BlobName);
-            }
-        }
+        //        _resourceUri = new Uri(BaseAddress, BlobName);
+        //    }
+        //}
 
-        private static async Task CreateBlobAsync(string dirPath, string filePath)
-        {
-            await CreateBlobAsync(dirPath, filePath, Encoding.UTF8.GetBytes(InitialContent));
-        }
+        //private static async Task CreateBlobAsync(string dirPath, string filePath)
+        //{
+        //    await CreateBlobAsync(dirPath, filePath, Encoding.UTF8.GetBytes(InitialContent));
+        //}
 
-        private static async Task CreateBlobAsync(string dirPath, string filePath, byte[] content)
-        {
-            var account = CloudStorageAccount.DevelopmentStorageAccount;
-            var client = account.CreateCloudBlobClient();
-            var container = client.GetContainerReference(ContainerName);
-            var directory = container.GetDirectoryReference(dirPath);
-            var blob = directory.GetBlockBlobReference(filePath);
+        //private static async Task CreateBlobAsync(string dirPath, string filePath, byte[] content)
+        //{
+        //    var account = CloudStorageAccount.DevelopmentStorageAccount;
+        //    var client = account.CreateCloudBlobClient();
+        //    var container = client.GetContainerReference(ContainerName);
+        //    var directory = container.GetDirectoryReference(dirPath);
+        //    var blob = directory.GetBlockBlobReference(filePath);
 
-            await blob.UploadFromByteArrayAsync(content, 0, content.Length);
-        }
+        //    await blob.UploadFromByteArrayAsync(content, 0, content.Length);
+        //}
     }
 }
---
+++ tests/NuGet.Services.Storage.Tests/AzureStorageNewFacts.cs
@@ -0,0 +1,62 @@
+﻿// Copyright (c) .NET Foundation. All rights reserved.
+// Licensed under the Apache License, Version 2.0. See License.txt in the project root for license information.
+
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Azure.Storage.Blobs;
+using Azure.Storage.Blobs.Specialized;
+using Azure.Storage.DataMovement.Blobs;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace NuGet.Services.Storage.Tests
+{
+    public class AzureStorageNewFacts
+    {
+        private AzureStorage storage;
+        private Mock<BlobClient> _blobClientMock = new Mock<BlobClient>();
+        private Mock<BlobServiceClient> _blobServiceClientMock = new Mock<BlobServiceClient>();
+        private Mock<BlobContainerClient> _blobContainerClientMock = new Mock<BlobContainerClient>();
+        private Mock<BlobsStorageResourceProvider> _blobStorageResourceProviderMock = new Mock<BlobsStorageResourceProvider>();
+        private Mock<ILogger<AzureStorage>> _loggerMock = new Mock<ILogger<AzureStorage>>();
+
+        public AzureStorageNewFacts() 
+        {   }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public void Exists(bool expected)
+        {
+            var azureResponse = new Mock<Azure.Response>();
+            _blobClientMock.Setup(c => c.Exists(It.IsAny<CancellationToken>())).Returns(Azure.Response.FromValue(expected, azureResponse.Object));
+            _blobContainerClientMock.Setup(bsc => bsc.GetBlobClient(It.IsAny<string>()))
+                .Returns(_blobClientMock.Object);
+            _blobServiceClientMock.Setup(bsc => bsc.GetBlobContainerClient(It.IsAny<string>())).Returns(_blobContainerClientMock.Object);
+            var azureStorage = new AzureStorage(_blobServiceClientMock.Object, _blobStorageResourceProviderMock.Object, "containerName", "path", new Uri("http://baseAddress"), useServerSideCopy: true, initializeContainer: true, _loggerMock.Object);
+
+            Assert.Equal(azureStorage.Exists("file"), expected);
+        }
+
+        [Theory]
+        [InlineData(true)]
+        [InlineData(false)]
+        public async Task ExistsAsync(bool expected)
+        {
+            var azureResponse = new Mock<Azure.Response>();
+            _blobClientMock.Setup(c => c.ExistsAsync(It.IsAny<CancellationToken>())).Returns(Task.FromResult(Azure.Response.FromValue(expected, azureResponse.Object)));
+            _blobContainerClientMock.Setup(bsc => bsc.GetBlobClient(It.IsAny<string>()))
+                .Returns(_blobClientMock.Object);
+            _blobServiceClientMock.Setup(bsc => bsc.GetBlobContainerClient(It.IsAny<string>())).Returns(_blobContainerClientMock.Object);
+            var azureStorage = new AzureStorage(_blobServiceClientMock.Object, _blobStorageResourceProviderMock.Object, "containerName", "path", new Uri("http://baseAddress"), useServerSideCopy: true, initializeContainer: true, _loggerMock.Object);
+
+            Assert.Equal(await azureStorage.ExistsAsync("file", CancellationToken.None), expected);
+        }
+    }
+}
